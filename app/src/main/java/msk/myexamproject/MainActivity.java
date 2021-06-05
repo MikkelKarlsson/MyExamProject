6de947752b314d0c9547375f4275daf09ed38341
MikkelKarlsson <55698695+MikkelKarlsson@users.noreply.github.com>
2021-06-05 10:02:24 +0200

    logged in

--- app/src/main/java/msk/myexamproject/MainActivity.java
+++ app/src/main/java/msk/myexamproject/MainActivity.java
@@ -1,15 +1,33 @@
 package msk.myexamproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView register;
+    private EditText editTextEmail, editTextPassword;
+    private Button signIn;
+
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +37,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //Initialize register button
         register = (TextView) findViewById(R.id.register);
         register.setOnClickListener(this);
+
+        //Initialize signIn button
+        signIn = (Button) findViewById(R.id.signIn);
+        signIn.setOnClickListener(this);
+
+        //Initialize email & password
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+
+        //Initialize progressbar
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        //Initialize mAuth
+        mAuth = FirebaseAuth.getInstance();
+
+
     }
 
     @Override
@@ -27,6 +61,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.register:
                 startActivity(new Intent(this, RegisterUser.class));
                 break;
+
+            case R.id.signIn:
+                userLogin();
+                break;
         }
+    }
+
+    private void userLogin() {
+        //Creating validation on username and password
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        //Checks if user has provided empty credentials, email
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        //If user provided valid email
+        if(Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please enter a valid email!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        //Checks if user has provided empty credentials, password
+        if(password.isEmpty()){
+            editTextPassword.setError("Password is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        //If user provided valid password length
+        if(password.length() < 6){
+            editTextPassword.setError("Min password length is 6 characters!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        //Spinning progressbar until user has logged in
+        progressBar.setVisibility(View.VISIBLE);
+
+        //Sign user in
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    //Redirect to user profile
+                    startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                }else{
+                    Toast.makeText(MainActivity.this, "Failed to login! Please check your credentials", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
